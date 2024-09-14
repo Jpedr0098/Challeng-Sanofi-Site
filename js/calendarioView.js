eb4474880f031f32ed8cebb97391e82f231ae77a
Jpedr0098 <joao.maurano.ismart@gmail.com>
2024-09-14 12:37:51 -0300

    Arumando abas do calendario - obs:falta a parte de edição do evento

--- js/calendarioView.js
+++ js/calendarioView.js
@@ -1,14 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      height: '700px',
-      fixedWeekCount: false,
-      events: []
+        initialView: 'dayGridMonth',
+        height: '700px',
+        fixedWeekCount: false,
+        events: [],
+
+        //Click na barra do evento
+        eventClick: function(info) {
+            var eventObj = info.event;
+            if (eventObj.url) {
+                alert(
+                'Clicked ' + eventObj.title + '.\n' +
+                'Will open ' + eventObj.url + ' in a new tab'
+                );
+
+                window.open(eventObj.url);
+
+                info.jsEvent.preventDefault(); // prevents browser from following link in current tab.
+            } else {
+                alert('Clicked ' + eventObj.title);
+            }
+        }
     });
+
     calendar.render();
 
-    // Função para adicionar eventos
+    //ADD eventos
     window.addEvent = function() {
     var title = document.getElementById('eventTitle').value;
     var date = document.getElementById('eventDate').value;
