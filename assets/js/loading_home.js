da88c5f1a5526f995d371b793d1ad3244c97cf7e
Jpedr0098 <joao.maurano.ismart@gmail.com>
2024-09-16 08:33:26 -0300

    ultimo ajuste das pastsa

--- assets/js/loading_home.js
+++ assets/js/loading_home.js
@@ -1,25 +1,25 @@
 const loginInfo = JSON.parse(localStorage.getItem("login")) || []
 const acessos = [
     {
-    acesso: '1',
-    txt: `<a href="./Malas.html"><img class="conf_malas" src=".\\assets\\conf_malas.png" alt="alt text" width="93%"/></a>`
+        acesso: '1',
+        txt: `<a href="./Malas.html"><img class="conf_malas" src=".\\assets\\images\\conf_malas.png" alt="alt text" width="93%"/></a>`
     },
     {
-    acesso: '2',
-    txt:`<a href="./Ia.html"><img class="conf_agil" src=".\\assets\\conf_agil.png" alt="alt text" width="93%"/></a>`
+        acesso: '2',
+        txt: `<a href="./Ia.html"><img class="conf_agil" src=".\\assets\\images\\conf_agil.png" alt="alt text" width="93%"/></a>`
     },
     {
-    acesso: '3',
-    txt:`<a href="https://app.powerbi.com/reportEmbed?reportId=3d920963-bde0-4787-8c91-1cff9a263c26&autoAuth=true&ctid=11dbbfe2-89b8-4549-be10-cec364e59551"><img class="dash_agil" src=".\\assets\\dash_agil.png" alt="alt text" width="93%"/></a>`
+        acesso: '3',
+        txt: `<a href="https://app.powerbi.com/reportEmbed?reportId=3d920963-bde0-4787-8c91-1cff9a263c26&autoAuth=true&ctid=11dbbfe2-89b8-4549-be10-cec364e59551"><img class="dash_agil" src=".\\assets\\images\\dash_agil.png" alt="alt text" width="93%"/></a>`
     }
 ]
 
-loginInfo.forEach(login => { 
+loginInfo.forEach(login => {
     let nivel = login.acess
 
     //redSenha(login.newAcess)
 
-    if (nivel == 'master'){
+    if (nivel == 'master') {
         cardMaster()
         acessos.forEach(cardInfo => card(cardInfo.txt))
     } else {
@@ -27,21 +27,21 @@ loginInfo.forEach(login => {
         console.log(niveis)
 
         acessos.forEach(cardInfo => {
-            if (niveis.includes(cardInfo.acesso)){
+            if (niveis.includes(cardInfo.acesso)) {
                 card(cardInfo.txt)
             }
         })
     }
 })
 
-function cardMaster(){
-    const cardTarefa = `<a href="./Usuarios_acessos.html"><img class="config" src="./assets/config.png" alt="alt text" /></a>`
+function cardMaster() {
+    const cardTarefa = `<a href="./Usuarios_acessos.html"><img class="config" src="./assets/images/config.png" alt="alt text" /></a>`
     const card = document.createElement("div")
     card.innerHTML = cardTarefa
     document.querySelector("#navBar").appendChild(card)
 }
 
-function card(cardInfo){
+function card(cardInfo) {
     const card = document.createElement("div")
     card.innerHTML = cardInfo
     document.querySelector("#btnNavSite").appendChild(card)
@@ -50,7 +50,7 @@ function card(cardInfo){
 // function redSenha(inputSenha){
 //     if (inputSenha == 1) {
 //         document.getElementById('modal').style.display = 'flex'
-//     }   
+//     }
 // }
 
 // document.getElementById('cancelar').addEventListener('click', function() {
